2f46251f3c4c097afe9ee53e27a5ad39de573c6a
osama ahmed <osamagamil2070@gmail.com>
2024-09-10 03:54:00 +0300

    update model

--- lib/features/doctor/data/model/attend_students_model.dart
+++ lib/features/doctor/data/model/attend_students_model.dart
@@ -7,24 +7,21 @@ class AttendStudentsModel extends AttendStudents {
   AttendStudentsModel(
       {required super.studentId,
       required super.studentname,
-      required super.collageName,
-      required super.facultyName,
+      required super.materialName,
       required super.studentLevel,
       required super.studentRound});
 
   AttendStudentsModel copyWith({
     int? studentId,
     String? studentname,
-    String? collageName,
-    String? facultyName,
+    String? materialName,
     int? studentLevel,
     int? studentRound,
   }) {
     return AttendStudentsModel(
       studentId: studentId ?? this.studentId,
       studentname: studentname ?? this.studentname,
-      collageName: collageName ?? this.collageName,
-      facultyName: facultyName ?? this.facultyName,
+      materialName: materialName ?? this.materialName,
       studentLevel: studentLevel ?? this.studentLevel,
       studentRound: studentRound ?? this.studentRound,
     );
@@ -32,23 +29,21 @@ class AttendStudentsModel extends AttendStudents {
 
   Map<String, dynamic> toMap() {
     return <String, dynamic>{
-      'user_id': studentId,
-      'user_name': studentname,
-      'college_name': collageName,
-      'faculty_name': facultyName,
-      'user_level': studentLevel,
-      'user_round': studentRound,
+      'studentId': studentId,
+      'studentname': studentname,
+      'materialName': materialName,
+      'studentLevel': studentLevel,
+      'studentRound': studentRound,
     };
   }
 
   factory AttendStudentsModel.fromMap(Map<String, dynamic> map) {
     return AttendStudentsModel(
-      studentId: map['user_id'] as int,
-      studentname: map['user_name'] as String,
-      collageName: map['college_name'] as String,
-      facultyName: map['faculty_name'] as String,
-      studentLevel: map['user_level'] as int,
-      studentRound: map['user_round'] as int,
+      studentId: map['studentId'] as int,
+      studentname: map['studentname'] as String,
+      materialName: map['materialName'] as String,
+      studentLevel: map['studentLevel'] as int,
+      studentRound: map['studentRound'] as int,
     );
   }
 
@@ -59,7 +54,7 @@ class AttendStudentsModel extends AttendStudents {
 
   @override
   String toString() {
-    return 'AttendStudentsModel(studentId: $studentId, studentname: $studentname, collageName: $collageName, facultyName: $facultyName, studentLevel: $studentLevel, studentRound: $studentRound)';
+    return 'AttendStudentsModel(studentId: $studentId, studentname: $studentname, materialName: $materialName, studentLevel: $studentLevel, studentRound: $studentRound)';
   }
 
   @override
@@ -68,8 +63,7 @@ class AttendStudentsModel extends AttendStudents {
 
     return other.studentId == studentId &&
         other.studentname == studentname &&
-        other.collageName == collageName &&
-        other.facultyName == facultyName &&
+        other.materialName == materialName &&
         other.studentLevel == studentLevel &&
         other.studentRound == studentRound;
   }
@@ -78,8 +72,7 @@ class AttendStudentsModel extends AttendStudents {
   int get hashCode {
     return studentId.hashCode ^
         studentname.hashCode ^
-        collageName.hashCode ^
-        facultyName.hashCode ^
+        materialName.hashCode ^
         studentLevel.hashCode ^
         studentRound.hashCode;
   }
--- lib/features/doctor/domain/entity/attend_students.dart
+++ lib/features/doctor/domain/entity/attend_students.dart
@@ -1,16 +1,14 @@
 class AttendStudents {
   final int studentId;
   final String studentname;
-  final String collageName;
-  final String facultyName;
+  final String materialName;
   final int studentLevel;
   final int studentRound;
 
   AttendStudents(
       {required this.studentId,
       required this.studentname,
-      required this.collageName,
-      required this.facultyName,
+      required this.materialName,
       required this.studentLevel,
       required this.studentRound});
 }
