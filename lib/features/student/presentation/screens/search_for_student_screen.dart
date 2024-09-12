4c469dae9cf6e20e8bd9fa416de2f8f0e61a4e3f
osama ahmed <osamagamil2070@gmail.com>
2024-09-12 20:14:08 +0300

    updates

---
+++ lib/features/student/presentation/screens/search_for_student_screen.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+import 'package:lock_doctors/core/common/widget/app_background_color.dart';
+
+class SearchForStudentScreen extends StatelessWidget {
+  const SearchForStudentScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: AppBackgroundColor(
+          screenContent: SafeArea(
+              child: Column(
+        children: [],
+      ))),
+    );
+  }
+}
---
+++ lib/features/student/presentation/widget/search_for_student_screen/custom_search_bar.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+
+import '../../../../../core/helpers/spacer.dart';
+import '../../../../../core/theme/app_pallete.dart';
+import '../../../../../core/theme/style.dart';
+
+class CustomSearchBar extends StatelessWidget {
+  const CustomSearchBar({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 36.h,
+      width: 320.w,
+      decoration: BoxDecoration(
+          border: Border.all(color: AppPallete.whiteColor),
+          borderRadius: BorderRadius.circular(40.r)),
+      child: Row(
+        children: [
+          horizontalSpace(22),
+          Image.asset(
+            "assets/image/Search_Image.png",
+            width: 18.75.w,
+            height: 18.72.w,
+          ),
+          horizontalSpace(12),
+          TextFormField(
+            decoration: InputDecoration(
+              border: InputBorder.none,
+              hintText: "Search for student",
+              hintStyle: TextStyles.font10WhiteRegular,
+            ),
+            style: TextStyles.font10WhiteRegular,
+          ),
+        ],
+      ),
+    );
+  }
+}
